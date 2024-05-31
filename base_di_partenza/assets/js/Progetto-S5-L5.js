2fe1e6d22c9a63218328e4b46cb1878b172ec43c
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-05-31 17:45:58 +0200

    add cambio colore della navbar allo scroll della pagina

--- base_di_partenza/assets/js/Progetto-S5-L5.js
+++ base_di_partenza/assets/js/Progetto-S5-L5.js
@@ -1,3 +1,9 @@
-const onscrollNavbar = function (e) {
-  console.log(e);
-};
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= 300) {
+    document.querySelector(".navBar").style.backgroundColor = "white";
+    document.querySelector(".navBar button").style.backgroundColor = "green";
+  } else {
+    document.querySelector(".navBar").style.backgroundColor = "#ffc017";
+    document.querySelector(".navBar button").style.backgroundColor = "black";
+  }
+});
