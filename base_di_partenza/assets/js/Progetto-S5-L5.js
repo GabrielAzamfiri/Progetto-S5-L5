d81ae659e990ba2117c37a9a7a48d12125a8d995
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-01 13:07:46 +0200

    aggiunta animazione su navBar e risolto problema dello scroll

--- base_di_partenza/assets/js/Progetto-S5-L5.js
+++ base_di_partenza/assets/js/Progetto-S5-L5.js
@@ -1,9 +1,19 @@
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset >= 300) {
-    document.querySelector(".navBar").style.backgroundColor = "white";
-    document.querySelector(".navBar button").style.backgroundColor = "green";
-  } else {
-    document.querySelector(".navBar").style.backgroundColor = "#ffc017";
-    document.querySelector(".navBar button").style.backgroundColor = "black";
+// grazie alla variabile fuori mi resistro il momento dello scroll a piu di 300px
+//cosi che solo in quel caso faccio partire l'animaizone di ritorno al colore originale
+let scrolPage = 0;
+window.onscroll = function () {
+  if (window.scrollY >= 300) {
+    document.querySelector(".navBar").classList.remove("animationToSenape");
+    document.querySelector(".navBar button").classList.remove("aminationToBlack");
+
+    document.querySelector(".navBar").classList.add("animationToWhite");
+    document.querySelector(".navBar button").classList.add("aminationToGreen");
+    scrolPage = window.scrollY;
+  } else if (window.scrollY < 300 && scrolPage >= 300) {
+    document.querySelector(".navBar").classList.remove("animationToWhite");
+    document.querySelector(".navBar button").classList.remove("aminationToGreen");
+
+    document.querySelector(".navBar").classList.add("animationToSenape");
+    document.querySelector(".navBar button").classList.add("aminationToBlack");
   }
-});
+};
